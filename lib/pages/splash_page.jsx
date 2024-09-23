b310d150bb37894d51632c4aaaf08893f306c292
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-23 19:54:06 -0300

    feat: create splash page

--- lib/pages/splash_page.jsx
+++ lib/pages/splash_page.jsx
@@ -1,13 +1,37 @@
-import { View, Text, Button } from "react-native"
+import { View, Image, StyleSheet } from 'react-native';
+import ButtonOutiline from '../core/components/button/button_outline';
 
-export default function SplashPage({navigation}){
+const SplashPage = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Splash page</Text>
-        <Button title="Ver jogadores" 
-        onPress={() => navigation.navigate('HomePage')}
-        
-        />
-    </View>
-    )
-}
+        <View style={styles.container}>
+            <Image
+                source={{ uri: 'https://img.freepik.com/fotos-premium/bola-de-volei-de-vista-superior-dentro-de-casa-natureza-morta_23-2150587341.jpg' }}
+                style={styles.backgroundImage}
+            />
+            <View style={styles.buttonContainer}>
+                <ButtonOutiline label="Ver jogadores" onPress={() => navigation.navigate('home')} />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backgroundImage: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+    },
+    buttonContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingBottom: 24,
+    },
+});
+
+export default SplashPage;
