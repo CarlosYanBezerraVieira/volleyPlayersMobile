d03fc7ef6534dbd8f551c39a69ae3bf71ef7746e
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-24 16:44:37 -0300

    feat: create loading

---
+++ lib/core/components/loading_spinner/LoadingSpinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+const LoadingSpinner = () => {
+  return (
+    <View style={styles.container}>
+      {/* Spinner com a cor personalizada */}
+      <ActivityIndicator size="large" color="#000" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5', // Definindo um fundo claro para contraste
+  },
+});
+
+export default LoadingSpinner;
