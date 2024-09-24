54a66f653ec5852787ea0c85a57cd254dab70c69
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-24 16:46:02 -0300

    feat: create home page

--- lib/pages/home_page.jsx
+++ lib/pages/home_page.jsx
@@ -1,9 +1,99 @@
-import { View, Text } from "react-native"
+import { useState, useEffect } from "react";
+import Header from "../core/components/header/Header.jsx";
+import volleyService from "../services/volei_service.js";
+import ListOfComponents from "../core/components/list_of_components/ListOfComponents.jsx";
+import LoadingSpinner from "../core/components/loading_spinner/LoadingSpinner.jsx";
 
-export default function wHomePage() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
-    )
+const HomePage = () => {
+  const [teams, setTeams] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
+
+  async function getData(params) {
+    setLoading(true); // Inicia o carregamento
+    try {
+      const data = await volleyService.getTeamsOfVolley(params);
+
+
+      console.log(data.response);
+      let value = data.response.map(team => {
+        return { ...team, isFavorited: false };
+      });
+      setTeams(value);
+    } catch (error) {
+      console.error("Erro ao buscar times de vôlei:", error);
+    } finally {
+      setLoading(false); // Finaliza o carregamento
+    }
+  }
+
+  const toggleFavorite = (newState, id) => {
+    const newTeams = teams.map(team =>
+      team.id === id ? { ...team, isFavorited: newState } : team
+    );
+    setTeams(newTeams);
+  };
+
+  const filterTeams = (isFavorited) => {
+    let data;
+
+    if (search === "") {
+      data = teams.filter(team => team.isFavorited === isFavorited);
+      return data;
+    }
+    data = teams.filter(team =>
+      team.isFavorited === isFavorited &&
+      team.name.toLowerCase().includes(search.toLowerCase())
+    );
+    return data;
+  };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  // useEffect(() => {
+  //   const noTeamsFound = filterTeams(false).length === 0 && filterTeams(true).length === 0;
+
+  //   if (noTeamsFound) {
+  //     if (search.length > 2) {
+  //       getData({ search });
+  //       console.log('Nenhum resultado encontrado, fazendo requisição com pesquisa...');
+  //     } else if (search.length > 2 && noTeamsFound) {
+  //       getData();
+  //       console.log('Nenhum resultado encontrado, carregando dados iniciais...');
+  //     }
+  //   }
+  // }, [search]);
+
+
+  return (
+    <>
+      <Header onChange={(value) => setSearch(value.target.value)} />
+      {loading ? (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: 1, marginLeft: '20px' }}>
+            <ListOfComponents
+              teams={filterTeams(false)}
+              onFavoriteToggle={toggleFavorite}
+              title="Times de Vôlei"
+            />
+          </div>
+          <div style={{ flex: 1, marginRight: '20px' }}>
+            <ListOfComponents
+              teams={filterTeams(true)}
+              onFavoriteToggle={toggleFavorite}
+              title="Times de Vôlei Favoritos"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
+
+export default HomePage;
